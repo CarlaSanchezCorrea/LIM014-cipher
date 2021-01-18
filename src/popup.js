c90b73a995fca793eb4eb44406880c197d0e5502
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-01-18 17:56:29 -0500

    Day 08

--- src/popup.js
+++
@@ -1,7 +0,0 @@
-
-var previousValue = localStorage.textarea == undefined ? '' : localStorage.textarea;
-document.getElementById('textarea').value = previousValue;
-
-function clear() {
-	document.getElementById('textarea').value='';
-}
