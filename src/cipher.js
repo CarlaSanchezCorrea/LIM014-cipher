fc4dc7d80d65be910767d4a8de3d886fcc312662
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-01-21 12:19:50 -0500

    21-Ene

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,30 @@
-const cipher = {
-  // ...
+/*Obtener valor del texto a ingresar*/
+var start = function (){
+    var texto = document.getElementById('input').value;
+/*Enviar TEXTO A INGRESAR -> OUTPUT*/   
+    document.getElementById('output').value =texto;
 };
 
-export default cipher;
+
+/*Obtener valor del shift*/
+var shift = function (){
+    var clave = document.getElementById('key').value;
+   
+    console.log(clave);
+};
+/*Cambiar cuando escoga BOTON ENCRYT & Oculta DECRYT*/
+function encrypt(){
+    var optionEncrypt = document.getElementById('encrypt');
+    var optionDecrypt = document.getElementById('decrypt');
+    optionDecrypt.innerHTML = '';
+    optionEncrypt.innerHTML = 'Encrypted';
+};
+/*Cambiar cuando escoga BOTON DECRYT & Oculta ENCRYT*/
+function decrypt(){
+    var optionDecrypt = document.getElementById('decrypt');
+    var optionEncrypt = document.getElementById('encrypt');
+    optionEncrypt.innerHTML = '';
+    optionDecrypt.innerHTML = 'Decrypted';
+};
+/*Enviar TEXTO A INGRESAR -> OUTPUT*/
+
