0f0c36504de2482bb311e023140645c07e7e952e
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-01-26 13:41:33 -0500

    26- January

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,43 @@
   
 const cipher = {
-    encode: function(offSet,encode){ /*To encrypt*/
-    let originalTxt = document.getElementById('input'); /*Gets the value of the input*/
-    let cipheredTxt = document.getElementById('output'); /*Gets the value of the output*/
-    let key = document.getElementById('key'); /*Gets the value of the shift*/
-    let encBtn = document.getElementById('enc'); /*Gets the value of the button Encrypt*/
-    let decBtn = document.getElementById('dec'); /*Gets the value of the button Decryt*/
-    cipheredTxt.value = "";  /*'Output' blank*/ 
-        
-        for (let i = 0; i < originalTxt.value.length; i++) { /*Loops throughh each letter of the "input"*/
-                if (originalTxt.value.charCodeAt(i) == 32) /*If there is blank space === charCodeAte(32)"*/
-                    cipheredTxt.value += String.fromCharCode(32) /*So on "output" skipped "" */
-                else {
-                    let ascii = originalTxt.value.charCodeAt(i) + Number(key.value); /*Numb.AsciiOriginal + Shift*/
-                    if (ascii > 122) 
-                        ascii = (ascii-65) % 26 + 65; /*Caesar Cipher's formula* Encrypted*/
-                        cipheredTxt.value += String.fromCharCode(ascii); /*Concatenate letter by letter*/            
-            }
+    encode: function(offSet,encode){ /*To encrypt*/    
+        /*console.log(typeof offSet, typeof encode)*/
+        if(offSet=== 0 || offSet== null){
+            throw TypeError()
         }
-        return cipheredTxt;
+        let resultado = "";
+        for (let i = 0; i < encode.length; i++) { /*Loops throughh each letter of the "input"*/
+                if (encode.charCodeAt(i) == 32) /*If there is blank space === charCodeAte(32)"*/
+                resultado += String.fromCharCode(32) /*So on "output" skipped "" */
+                else {
+                    let ascii = (encode.charCodeAt(i) + offSet -65) %26 +65; /*Numb.AsciiOriginal + Shift*/
+                    if (ascii > 122) 
+                         ; /*Caesar Cipher's formula* Encrypted*/
+                        resultado += String.fromCharCode(ascii); /*Concatenate letter by letter*/            
+                    }
+        }
+        return resultado; 
     } ,
 
 
 
-        decode: function(offSet,decode){ /*To decrypt*/
-        let originalTxt = document.getElementById('input'); /*Gets the value of the input*/
-        let cipheredTxt = document.getElementById('output'); /*Gets the value of the output*/
-        let key = document.getElementById('key'); /*Gets the value of the shift*/
-        let encBtn = document.getElementById('enc'); /*Gets the value of the button Encrypt*/
-        let decBtn = document.getElementById('dec'); /*Gets the value of the button Decryt*/
-        cipheredTxt.value = "";  
-            
-            for (let i = 0; i < originalTxt.value.length; i++) { /*Loops throughh each letter of the "input"*/
-                    if (originalTxt.value.charCodeAt(i) == 32) /*If there is blank space === charCodeAte(32)"*/
-                        cipheredTxt.value += String.fromCharCode(32) /*So on "output" skipped "" */
-                    else {
-                        let ascii = originalTxt.value.charCodeAt(i) - Number(key.value); /*Numb.AsciiOriginal + Shift*/
-                        if (ascii > 122) 
-                            ascii = ((ascii-65) % 26) + 65; /*Caesar Cipher's formula Decryted*/
-                            cipheredTxt.value += String.fromCharCode(ascii); /*Concatenate letter by letter*/            
-                }
-            }
-            return cipheredTxt;
+    decode: function(offSet,decode){ /*To encrypt*/    
+        /*console.log(typeof offSet, typeof encode)*/
+        if(offSet=== 0 || offSet== null){
+            throw TypeError()
+        }
+        let resultado = "";
+        for (let i = 0; i < decode.length; i++) { /*Loops throughh each letter of the "input"*/
+                if (decode.charCodeAt(i) == 32) /*If there is blank space === charCodeAte(32)"*/
+                resultado += String.fromCharCode(32) /*So on "output" skipped "" */
+                else {
+                    let ascii = (decode.charCodeAt(i) - offSet +65) %26 +65; /*Numb.AsciiOriginal + Shift*/
+                    if (ascii > 122) 
+                         ; /*Caesar Cipher's formula* Encrypted*/
+                        resultado += String.fromCharCode(ascii); /*Concatenate letter by letter*/            
+                    }
+        }
+        return resultado; 
         } 
     };
   
@@ -49,6 +45,3 @@ const cipher = {
 
 
   export default cipher;
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
+  
 import cipher from './cipher.js';
 
-console.log(cipher);
     let originalTxt = document.getElementById('input'); /*Gets the value of the input*/
     let cipheredTxt = document.getElementById('output'); /*Gets the value of the output*/
     let key = document.getElementById('key'); /*Gets the value of the shift*/   
     let encBtn = document.getElementById('enc'); /*Gets the value of the button Encrypt*/
     let decBtn = document.getElementById('dec'); /*Gets the value of the button Decryt*/
     let text = ""; /*'Output' blank*/ 
+    let offSet = Number(key.value)
 
-    encBtn.addEventListener('click', function (e) {  /*To Button Encryt*/ 
-        let encode = originalTxt.value;  
-        let offSet = Number(key.value); 
+    encBtn.addEventListener('click', function () {  /*To Button Encryt*/ 
+        let encode1 = originalTxt.value; 
+        let encode= encode1.toUpperCase(); 
+        /*let offSet = Number(key.value);*/
+        /*let  resultado =cipheredTxt.value;*/
         text = cipher.encode(offSet,encode);
-        document.getElementById("output").innerHTML = text;
+        /*resultado = text;*/
+        console.log(text) ;
+        cipheredTxt.value = text;   
+     }, true);
 
-        
-     }, false);
 
-
-     decBtn.addEventListener('click', function (e) { /*To Button Decrypt*/
-        let decode = originalTxt.value;
-        let offSet = Number(key.value); 
-        text = cipher.decode(offSet,decode);
-        document.getElementById("output").innerHTML = text;
-
-        
-     }, false);
-     
-
+     decBtn.addEventListener('click', function () { /*To Button Decrypt*/
+         let encode1 = originalTxt.value; 
+         let decode= encode1.toUpperCase(); 
+         /*let offSet = Number(key.value);*/
+         /*let  resultado =cipheredTxt.value;*/
+         text = cipher.decode(offSet,decode);
+         /*resultado = text;*/
+         console.log(text) ;
+         cipheredTxt.value = text;
+      }, false);
