d310ce6728136539a7d8d85981485d2710c2a794
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-01-28 17:10:47 -0500

    28.Ene

--- src/index.js
+++ src/index.js
@@ -7,27 +7,27 @@ import cipher from './cipher.js';
     let encBtn = document.getElementById('enc'); /*Gets the value of the button Encrypt*/
     let decBtn = document.getElementById('dec'); /*Gets the value of the button Decryt*/
     let text = ""; /*'Output' blank*/ 
-    let offSet = Number(key.value)
+    /*let offSet = Number(key.value);*/
 
     encBtn.addEventListener('click', function () {  /*To Button Encryt*/ 
         let encode1 = originalTxt.value; 
         let encode= encode1.toUpperCase(); 
-        /*let offSet = Number(key.value);*/
+        let offSet = Number(key.value);
         /*let  resultado =cipheredTxt.value;*/
         text = cipher.encode(offSet,encode);
         /*resultado = text;*/
-        console.log(text) ;
+        /*console.log(text) ;*/
         cipheredTxt.value = text;   
      }, true);
 
 
      decBtn.addEventListener('click', function () { /*To Button Decrypt*/
-         let encode1 = originalTxt.value; 
-         let decode= encode1.toUpperCase(); 
-         /*let offSet = Number(key.value);*/
+         let decode1 = originalTxt.value; 
+         let decode= decode1.toUpperCase(); 
+         let offSet = Number(key.value);
          /*let  resultado =cipheredTxt.value;*/
          text = cipher.decode(offSet,decode);
          /*resultado = text;*/
-         console.log(text) ;
+         /*console.log(text);*/
          cipheredTxt.value = text;
       }, false);
